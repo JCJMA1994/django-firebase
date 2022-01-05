1591d67a3acc74651c3f8a9fac0045ef2bff02f2
JCJMA1994 <201214026@uns.edu.pe>
2022-01-05 17:31:38 -0500

    Modified view welcome.html, signup.html

--- website/urls.py
+++ website/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
 
     # url
     path('', auth_views.LoginView.as_view(template_name='signIn.html'), name='home'),
+    path('logout/', auth_views.LoginView.as_view(template_name='signIn.html'), name="logout"),
+
     path('signin/', views.signIn, name='signIn'),
     path('signup/', views.signUp, name='signUp'),
     path('postsign/', views.postSignIn, name='postSignIn'),
-    path('logout/', auth_views.LoginView.as_view(template_name='signIn.html'), name="logout"),
     path('postsignup/', views.postSignUp, name="postSignUp"),
 
 ]
